c1b12a7189c89bf718fecd5411432029a627407c
Alexsandro Rocha <alexrocha.f@outlook.com>
2016-10-22 19:41:33 -0200

    PIM-$

---
+++ thehost/thehost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using thehost.Formularios;
+
+namespace thehost
+{
+    static class Program
+    {
+        /// <summary>
+        /// Inicialização do programa.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmPrincipal());
+        }
+    }
+}
